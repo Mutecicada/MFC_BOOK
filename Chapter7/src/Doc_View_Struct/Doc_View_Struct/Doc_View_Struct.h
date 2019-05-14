e635463c9d68cf05b1f8069752db2fcc690faf2e
Mutecicada <mutecicada@naver.com>
2019-05-14 17:15:33 +0900

    Chapter 7

---
+++ Chapter7/src/Doc_View_Struct/Doc_View_Struct/Doc_View_Struct.h
@@ -0,0 +1,32 @@
+﻿
+// Doc_View_Struct.h: Doc_View_Struct 응용 프로그램의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'stdafx.h'를 포함합니다."
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CDocViewStructApp:
+// 이 클래스의 구현에 대해서는 Doc_View_Struct.cpp을(를) 참조하세요.
+//
+
+class CDocViewStructApp : public CWinApp
+{
+public:
+	CDocViewStructApp() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDocViewStructApp theApp;
---
+++ Chapter7/src/Doc_View_Struct/Doc_View_Struct/Doc_View_StructView.h
@@ -0,0 +1,49 @@
+﻿
+// Doc_View_StructView.h: CDocViewStructView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CDocViewStructView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CDocViewStructView() noexcept;
+	DECLARE_DYNCREATE(CDocViewStructView)
+
+// 특성입니다.
+public:
+	CDocViewStructDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CDocViewStructView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // Doc_View_StructView.cpp의 디버그 버전
+inline CDocViewStructDoc* CDocViewStructView::GetDocument() const
+   { return reinterpret_cast<CDocViewStructDoc*>(m_pDocument); }
+#endif
+
